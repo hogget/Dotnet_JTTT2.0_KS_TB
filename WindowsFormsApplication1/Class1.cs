d074430dd9d84f5e1b85a23c951ee86fac08da5c
klaudia <klaudia.siedlarz@gmail.com>
2016-03-20 01:43:20 +0100

    HackathonWersja2.0

---
+++ WindowsFormsApplication1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Task
+    {
+        public string Adres_strony;
+        public string Klucz;
+        public string Email;
+        public Task(string Adres, string klucz, string email)
+        {
+            Adres_strony = Adres;
+            Klucz = klucz;
+            Email = email;
+        }
+        public Task() { }
+    }
+}
